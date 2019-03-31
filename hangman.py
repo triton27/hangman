b20d35892f2dc00617035fe1ddc17a20ab49d64e
toriumi <na54heart@icloud.com>
2019-04-01 00:49:19 +0900

    Add hangman

---
+++ hangman.py
@@ -0,0 +1,15 @@
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
